13d5c0f0805ef8d35b7d7ab06f6fa3749ecdfc4d
yehuya <yehuda@k.co.il>
2016-08-25 08:56:26 +0300

    update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ var order = require('gulp-order');
 var uglify = require('gulp-uglify')
 
 gulp.task('js', function(){
-	return gulp.src(['src/*.js', '!frontendEditor.concat.js'])
+	return gulp.src(['src/*.js', '!src/frontendEditor.concat.js'])
 	.pipe(order([
+        "text.js",
 		"main.js"
 	]))
 	.pipe(concat('frontendEditor.concat.js'))
@@ -16,4 +17,6 @@ gulp.task('js', function(){
 	.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['js']);
+gulp.task('default', function(){
+    gulp.watch(['src/*.js', '!src/frontendEditor.concat.js'], ['js']);
+});
