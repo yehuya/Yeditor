148d6b6bf48ddbf36853811194ed1aa1f74484ca
yehuya <yehuda@k.co.il>
2016-08-25 11:43:01 +0300

    add sass + cssmin gulp module

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,37 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var order = require('gulp-order');
-var uglify = require('gulp-uglify')
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var sass = require('gulp-sass');
 
 gulp.task('js', function(){
-	return gulp.src(['src/*.js', '!src/frontendEditor.concat.js'])
+	return gulp.src(['src/js/*.js', '!src/js/frontendEditor.concat.js'])
 	.pipe(order([
         "text.js",
 		"main.js"
 	]))
 	.pipe(concat('frontendEditor.concat.js'))
-	.pipe(gulp.dest('src'))
+	.pipe(gulp.dest('src/js'))
 	.pipe(rename('frontendEditor.min.js'))
 	.pipe(uglify())
 	.pipe(gulp.dest('dist'));
 });
 
+gulp.task('sass', function(){
+	return gulp.src('src/sass/*.sass')
+	.pipe(sass())
+	.pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('css', ['sass'], function(){
+	return gulp.src(['src/css/*.css'])
+	.pipe(cssmin())
+	.pipe(rename('frontendEditor.css.js'))
+	.pipe(gulp.dest('dist'));
+});
+
 gulp.task('default', function(){
-    gulp.watch(['src/*.js', '!src/frontendEditor.concat.js'], ['js']);
+    gulp.watch(['src/js/*.js', '!src/js/frontendEditor.concat.js'], ['js']);
+	gulp.watch(['src/sass/*.sass'], ['css']);
 });
