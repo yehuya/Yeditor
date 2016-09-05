4124001e7fa1442d122ae7f80ff539ae9efa7413
yehuda <yehuda.yadid@gmail.com>
2016-09-05 22:42:56 +0300

    keep working

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -47,8 +47,12 @@
 	'use strict';
 
 	var Selection = __webpack_require__(1).default;
+	var Editable = __webpack_require__(2).default;
+	var Editor = __webpack_require__(4).default;
 
 	window.selection = new Selection();
+	window.editable = new Editable();
+	window.editor = new Editor();
 
 /***/ },
 /* 1 */
@@ -160,6 +164,280 @@
 	}();
 
 	exports.default = Selection;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _config = __webpack_require__(3);
+
+	var _config2 = _interopRequireDefault(_config);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	/**
+	 * get all the editable area
+	 */
+	var Editable = function () {
+
+	    /**
+	     * __construct
+	     * set param from config file
+	     */
+	    function Editable() {
+	        _classCallCheck(this, Editable);
+
+	        var config = _config2.default.editable;
+	        this.htmlTag = config.htmlTag;
+	        this.nameAttribute = config.nameAttribute;
+	    }
+
+	    /**
+	     * get all editable area
+	     * @return Array 
+	     */
+
+
+	    _createClass(Editable, [{
+	        key: 'get',
+	        value: function get() {
+	            var allEditArea = document.querySelectorAll(this.htmlTag);
+	            return allEditArea;
+	        }
+
+	        /**
+	         * set 'editable area' as editable
+	         */
+
+	    }, {
+	        key: 'set',
+	        value: function set() {
+	            var allEditArea = this.get();
+	            allEditArea.forEach(function (element) {
+	                element.setAttribute('contenteditable', 'true');
+	            }, this);
+	        }
+
+	        /**
+	         * unset 'editable area' as editable 
+	         */
+
+	    }, {
+	        key: 'unset',
+	        value: function unset() {
+	            var allEditArea = this.get();
+	            allEditArea.forEach(function (element) {
+	                element.removeAttribute('contenteditable');
+	            }, this);
+	        }
+	    }]);
+
+	    return Editable;
+	}();
+
+	exports.default = Editable;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var _exports = module.exports;
+	var prefix = 'frontendEditor-';
+
+	/**
+	 * config for Editable.class.js
+	 */
+	_exports.editable = {
+	  htmlTag: 'edit',
+	  nameAttribute: 'name'
+	};
+
+	/**
+	 * config for editor/Nav.class.js
+	 */
+	_exports.nav = {
+	  navClass: prefix + 'area-nav',
+	  navId: prefix + 'nav'
+	};
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _ButtonClass = __webpack_require__(5);
+
+	var _ButtonClass2 = _interopRequireDefault(_ButtonClass);
+
+	var _NavClass = __webpack_require__(7);
+
+	var _NavClass2 = _interopRequireDefault(_NavClass);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var Editor = function Editor() {
+	    _classCallCheck(this, Editor);
+
+	    this.Button = new _ButtonClass2.default();
+	    this.Nav = new _NavClass2.default();
+	};
+
+	exports.default = Editor;
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _config = __webpack_require__(3);
+
+	var _config2 = _interopRequireDefault(_config);
+
+	var _button = __webpack_require__(6);
+
+	var _button2 = _interopRequireDefault(_button);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	/**
+	 * editor button class
+	 */
+	var Button = function () {
+	    function Button() {
+	        _classCallCheck(this, Button);
+
+	        this.btn_image = _button2.default.image;
+	        this.btn_text = _button2.default.text;
+	    }
+
+	    /**
+	     * event onClick on button
+	     * @param Object (button)
+	     * @Param FN (callback function)
+	     */
+
+
+	    _createClass(Button, [{
+	        key: 'click',
+	        value: function click(button, callback) {
+	            button.addEventListener('click', function (e) {
+	                e.preventDefault();
+	                if (typeof callback == 'function') {
+	                    callback();
+	                }
+	            });
+	        }
+	    }]);
+
+	    return Button;
+	}();
+
+	exports.default = Button;
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var _exports = module.exports;
+
+	_exports.text = [];
+
+	_exports.image = [];
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _config = __webpack_require__(3);
+
+	var _config2 = _interopRequireDefault(_config);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var Nav = function () {
+
+	    /**
+	     * __construct
+	     */
+	    function Nav() {
+	        _classCallCheck(this, Nav);
+
+	        this.config = _config2.default.nav;
+	        this.appendToDocument(this.element());
+	    }
+
+	    /**
+	     * create main navigation element (DOM)
+	     * @return Object
+	     */
+
+
+	    _createClass(Nav, [{
+	        key: 'element',
+	        value: function element() {
+	            var element = document.createElement('nav');
+	            element.classList.add(this.config.navClass);
+	            element.id = this.config.navId;
+
+	            return element;
+	        }
+
+	        /**
+	         * append DOM element into the document.body
+	         * @param Object (DOM element)
+	         */
+
+	    }, {
+	        key: 'appendToDocument',
+	        value: function appendToDocument(Node) {
+	            document.body.appendChild(Node);
+	        }
+	    }]);
+
+	    return Nav;
+	}();
+
+	exports.default = Nav;
 
 /***/ }
 /******/ ]);
