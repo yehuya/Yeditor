7c13b4bc9bdb59233de7ea9fe9fc11400b4b63d3
yehuda <yehuda.yadid@gmail.com>
2016-09-05 22:55:35 +0300

    change require class to import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const Selection = require('./text/Selection.class.js').default;
-const Editable = require('./Editable.class.js').default;
-const Editor = require('./editor/Editor.class.js').default;
+import Selection from './text/Selection.class.js';
+import Editable from './Editable.class.js';
+import Editor from './editor/Editor.class.js';
 
 window.selection = new Selection();
 window.editable = new Editable();
