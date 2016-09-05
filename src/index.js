67901ec40f9296971ea7778e4e3c8d2e3894cd59
yehuda <yehuda.yadid@gmail.com>
2016-09-05 22:42:41 +0300

    test new classes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const Selection = require('./text/selection.class.js').default;
+const Selection = require('./text/Selection.class.js').default;
+const Editable = require('./Editable.class.js').default;
+const Editor = require('./editor/Editor.class.js').default;
 
 window.selection = new Selection();
+window.editable = new Editable();
+window.editor = new Editor();
 
