4a8e99d13e42215349507366aff41dbf2af953be
yehuda <yehuda.yadid@gmail.com>
2016-09-05 22:55:11 +0300

    add buttons

--- src/editor/button.js
+++ src/editor/button.js
@@ -1,5 +1,48 @@
 const exports = module.exports;
 
-exports.text = [];
+exports.text = [
+    /**
+     * bold
+     * @DOM span
+     * @CSS font-weight="bold"
+     */
+    {
+        name: 'bold',
+        node: function(){
+            var element = document.createElement('span');
+            element.style.fontWeight = 'bold';
+
+            return element.cloneNode();
+        }
+    },
+    /**
+     * italic
+     * @DOM span
+     * @CSS font-style="italic"
+     */
+    {
+        name: 'italic',
+        node: function(){
+            var element = document.createElement('span');
+            element.style.fontStyle = 'italic';
+
+            return element.cloneNode();
+        }
+    },
+    /**
+     * underline
+     * @DOM span
+     * @CSS text-decoration="underline"
+     */
+    {
+        name: 'underline',
+        node: function(){
+            var element = document.createElement('span');
+            element.style.textDecoration = 'underline';
+
+            return element.cloneNode();
+        }
+    }
+];
 
 exports.image = [];
