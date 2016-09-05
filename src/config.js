7e61c26605fc65b5e68bb311369b8ca1ba3c6d50
yehuda <yehuda.yadid@gmail.com>
2016-09-05 22:43:30 +0300

    create config file for the plugin

---
+++ src/config.js
@@ -0,0 +1,19 @@
+const exports = module.exports;
+const prefix = 'frontendEditor-';
+
+/**
+ * config for Editable.class.js
+ */
+exports.editable = {
+    htmlTag: 'edit',
+    nameAttribute: 'name'
+}
+
+/**
+ * config for editor/Nav.class.js
+ */
+exports.nav = {
+    navClass: `${prefix}area-nav`,
+    navId: `${prefix}nav`
+}
+
