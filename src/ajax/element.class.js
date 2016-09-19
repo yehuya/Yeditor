515e8b39e0c2668f22a97a7835ac3abe8627bebe
yehuda <yehuda.yadid@gmail.com>
2016-09-19 11:20:02 +0300

    create ajax classes

---
+++ src/ajax/element.class.js
@@ -0,0 +1,10 @@
+import Config from './../config.js';
+
+/**
+ * get all editable elements
+ */
+export default class Element {
+    constructor(){
+        this.config = Config.ajax.element;
+    }
+} 
