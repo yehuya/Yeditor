82e272e139547ea860921fb3143c6d1edcefdef2
yehuda <yehuda.yadid@gmail.com>
2016-09-19 12:05:01 +0300

    add element.class.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Selection from './text/Selection.class.js';
 import Editable from './Editable.class.js';
 import Editor from './editor/Editor.class.js';
+import Element from './element/Element.class.js';
 
 window.selection = new Selection();
 window.editable = new Editable();
 window.editor = new Editor();
+window.element = new Element()
 
 // usefull link
 // https://html.spec.whatwg.org/multipage/interaction.html#attr-contenteditable
