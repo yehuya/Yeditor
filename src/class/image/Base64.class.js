536e2f9540deced9e374f94d8e217b4eae2c7632
yehuda <yehuda.yadid@gmail.com>
2016-09-22 00:25:45 +0300

    create images class test some blob function in test files move the css from index.html into style.css

---
+++ src/class/image/Base64.class.js
@@ -0,0 +1,11 @@
+/**
+ * create image src as base64
+ * set base64 as binary
+ */
+export default class Base64 {
+    constructor(){}
+
+    blob(){
+        
+    }
+}
---
+++ src/class/image/Render.class.js
@@ -0,0 +1,18 @@
+/**
+ * render image object
+ */
+export default class Render {
+    constructor(){}
+
+    /**
+     * create Image object
+     */
+    create(){
+        var img = new Image();
+        return img;
+    }
+
+    /**
+     * render image
+     */
+}
