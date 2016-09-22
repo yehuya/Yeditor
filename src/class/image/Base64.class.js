3154cb3d1f4020911bd5ef8b1496ba703296f781
yehuda <yehuda.yadid@gmail.com>
2016-09-23 01:48:03 +0300

    update image classes structure

--- src/class/image/Base64.class.js
+++ src/class/image/Base64.class.js
@@ -3,9 +3,7 @@
  * set base64 as binary
  */
 export default class Base64 {
-    constructor(){
-
-    }
+    constructor(){}
 
     /**
      * clean base64 string from "data:image/*;base64,"
@@ -13,5 +11,18 @@ export default class Base64 {
      */
     clean(b64){
         return b64.substring(b64.indexOf(',') + 1);
+    }
+
+    /**
+     * get image file and return it as base64 data url
+     * @param Object (file)
+     * @param FN (callback: url: String, file: Object)
+     */
+    image(file, callback){
+        const reader = new FileReader();
+        reader.onload = function(event){
+            callback(event.target.result, file);
+        }
+        reader.readAsDataURL(file);
     }
 }
