542e2a79f76e4d02acf973fa4cafbd602fcc4a81
yehuda <yehuda.yadid@gmail.com>
2016-09-23 01:49:22 +0300

    remove render class

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Editor from './class/editor/Editor.class.js';
 import Element from './class/Element.class.js';
 import Ajax from './class/ajax/Ajax.class.js';
 import Serialize from './class/ajax/Serialize.class.js';
-import Render from './class/image/Render.class.js';
 
 window.selection = new Selection();
 window.editable = new Editable();
@@ -12,7 +11,6 @@ window.editor = new Editor();
 window.element = new Element();
 window.ajax = new Ajax();
 window.serialize = new Serialize();
-window.render = new Render();
 
 // usefull link
 // https://html.spec.whatwg.org/multipage/interaction.html#attr-contenteditable
