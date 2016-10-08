98f88a5d5841a483ea9b3693c28695990f9491e2
yehuda <yehuda.yadid@gmail.com>
2016-10-09 00:07:50 +0300

    keep working on text selection

--- test/js/script.js
+++ test/js/script.js
@@ -3,16 +3,13 @@ function detect() {
     if (selection.isCollapsed) return alert('Select some text');
 
     var range = selection.getRangeAt(0);
-
+    window.r = range;
     var start = range.startContainer;
     var end = range.endContainer;
     var offset = { start: range.startOffset, end: range.endOffset }
-    var sibling = {start: range.startContainer.nextSibling ,end: range.endContainer.previousSibling}
-
-    process(start, end, sibling, offset, range);
-    console.info('end process');
-    console.log('sibling start', sibling.start);
-    console.log('sibling end', sibling.end);
+    var sibling = { start: range.startContainer.nextSibling, end: range.endContainer.previousSibling }
+    console.log(sibling);
+    process2(start, end, sibling, offset, range);
 }
 
 function selection() {
@@ -22,44 +19,121 @@ function selection() {
     s.removeAllRanges();
 
     return range;
+};
+
+function process2(start, end, sibling, offset, range) {
+    var startElement, endElement;
+
+    // same element
+    if (start == end) {
+        startElement = appendFromTo(start, offset.start, offset.end);
+        console.log('1');
+
+    // not the same element
+    } else {
+        startElement = appendFromTo(start, offset.start, null);
+        endElement = appendFromTo(end, null, offset.end);
+        console.log('2');
+    }
+
+    // refresh sibling after the first appendFromTo fn
+    // prevent junk sibling
+    if (sibling.start == null) sibling.start = preventEmptySibling(range.startContainer.nextSibling);
+    if (sibling.end == null) sibling.end = preventEmptySibling(range.endContainer.previousSibling);
+
+    // only 'one' element between end & start
+    if (sibling.start == sibling.end && sibling.start != null) {
+        console.log('3', sibling.start);
+        return append(sibling.start);
+    }
+
+    // get all the element between
+    var next = sibling.start;
+    var a, b, c;
+
+    while (next) {
+        console.log('4', next);
+
+        // ### children
+        b = false;
+        a = children(next, function (elem) {
+            if (!b) append(elem);
+            if (elem == sibling.end) b = true;
+            console.log('4.1', elem, b);
+        });
+
+        // ### no children
+        if (!a && !b) {
+            console.log('4.2', next);
+            next = append(next);
+        }
+
+        // ### break
+        if (next == end.toString()) {
+            console.log('4.3.0');
+        }
+
+        if (next == sibling.end.toString()) {
+            console.log('4.3.0.1');
+            if (end.textContent.toString().substring(0, offset.end) == next.textContent) break;
+            console.log('4.3.0.1.0')
+        }
+
+        // child equal to sibling.end
+        if (b) {
+            console.log('4.3.1');
+            break;
+        }
+
+        if (next == sibling.end) {
+            console.log('4.3.2');
+            break;
+        }
+
+        // ### next while
+        next = next.nextSibling;
+        if (next == null) {
+            console.log('just end');
+        }
+    }
 }
 
 function process(start, end, sibling, offset, range) {
     if (start == end) {
+        console.log(1);
         appendFromTo(start, offset.start, offset.end);
     } else {
+        console.log(2);
         appendFromTo(start, offset.start, null);
         appendFromTo(end, null, offset.end);
 
         if (sibling.start == sibling.end) {
             append(sibling.start);
+            console.log(3);
+
         } else {
             var next = sibling.start;
+            console.log(4);
 
             while (next) {
                 var b = false;
                 var a = children(next, function (elem) {
                     append(elem);
-                    console.log('children',elem);
+                    console.log(5);
                     if (elem == sibling.end) b = true;
                 });
 
-                if(!a){
-                    console.log('no children');
+                if (!a) {
                     var c = append(next);
-                    // next = c;
-                    // console.log('test', next, next.nextSibling);
-                }else{
-                    console.log('has children');
+                    console.log(6);
                 }
 
                 if (b || next == sibling.end) {
+                    console.log(7);
                     break;
                 }
 
-                console.log('current', next)
                 next = next.nextSibling;
-                console.log('end while - the next', next);
             }
         }
     }
@@ -68,7 +142,7 @@ function process(start, end, sibling, offset, range) {
 // get all element - children
 function children(node, callback) {
     var child = node.childNodes;
-    if(child.length == 0) return false;
+    if (child.length == 0) return false;
 
     for (var i = 0; i < child.length; i++) {
         if (child[i].children) {
@@ -88,8 +162,8 @@ function append(node) {
 // @param Object (node)
 // @return Object (the element with his parent - span);
 function appendFromTo(node, from, to) {
-    var text = node.textContent;
-    var parent = node.parentElement;
+    var text = node.textContent || '';
+    var parent = node.parentElement || null;
     var nextSibling = node.nextSibling;
 
     if (to == null) to = text.length;
@@ -122,4 +196,17 @@ function appendTextToSpan(text) {
     span.appendChild(textNode);
 
     return span.cloneNode(true)
+}
+
+// prevent empty sibling 
+// set theme as null
+function preventEmptySibling(elem) {
+    if (elem.nodeType == 3) {
+        if (elem.data) {
+            if (elem.data.trim().length == 0) {
+                return null;
+            }
+        }
+    }
+    return elem;
 }
